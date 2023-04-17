2e267078af5140ddc978e3aab396f4e2c4237558
Vaisakh V N <sadaimudinaadhar@gmail.com>
2023-04-17 02:56:52 +0200

    update test

--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -1,6 +1,46 @@
+import { getConnection } from "../../src/infrastructure/db/Connection";
+import { TruckModel } from "../../src/models";
+import { repoCreateTruck, repoGetTruck } from "../../src/repositories/TruckRepository";
 
-console.log( process.env.NODE_ENV)
-test('database connection', async () => {
+jest.mock("../../src/infrastructure/db/Connection", () => ({
+  getConnection: jest.fn(() => Promise.resolve({
+    getRepository: jest.fn(),
+  })),
+}));
 
-    expect(2).toBe(2);
+describe("TruckRepository", () => {
+  describe("repoGetTruck", () => {
+    it("should call getRepository with the correct arguments", async () => {
+      const mockGetRepository = jest.fn();
+      (getConnection as jest.Mock).mockImplementationOnce(() => Promise.resolve({
+        getRepository: mockGetRepository,
+      }));
+
+      const id = 1;
+      await repoGetTruck(id);
+
+      expect(mockGetRepository).toHaveBeenCalledWith(id);
+    });
   });
+
+  describe("repoCreateTruck", () => {
+    it("should call getRepository with the correct arguments", async () => {
+      const mockGetRepository = jest.fn();
+      (getConnection as jest.Mock).mockImplementationOnce(() => Promise.resolve({
+        getRepository: mockGetRepository,
+      }));
+
+      const truck: TruckModel = {
+        brand: "sadsad",
+        load: 1,
+        capacity: 1,
+        year: 1,
+        numRepairs: 1,
+      };
+      await repoCreateTruck(truck);
+
+      expect(mockGetRepository).toHaveBeenCalledWith(truck);
+    });
+  });
+});
+
